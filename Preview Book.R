09b5f906cf8fc6016c09a20d1d1160ea531dc9cf
matachingames <59778388+matachingames@users.noreply.github.com>
2021-03-19 00:39:02 -0400

    Formatting update

--- Preview Book.R
+++ Preview Book.R
@@ -5,8 +5,8 @@ library(bookdown)
 serve_book(dir = ".", output_dir = "docs", preview = TRUE, 
            in_session = TRUE, quiet = FALSE)
 
-# Stop servr
-servr::daemon_stop(1)
-
 # Live version of book
 http://127.0.0.1:4321
+
+# Stop servr
+servr::daemon_stop(1)
